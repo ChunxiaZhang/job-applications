fb5b56b2e31d0ec3936656642e4a8ddddb8c2ca5
ChunxiaZhang <persephonezcx@gmail.com>
2015-11-12 14:02:03 -0500

    add all files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jobApplicationsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jobApplicationsApp
+ */
+angular.module('jobApplicationsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
